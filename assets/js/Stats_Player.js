5da9a21d7918aa6cd60d964a79e38f3d9e15fee5
Vinnie <vmatronia@gmail.com>
2025-08-28 22:09:31 -0400

    Create Stats_Player.js

---
+++ assets/js/Stats_Player.js
@@ -0,0 +1,19 @@
+/* GawsStats Player Stats */
+
+var PLAYER_UUID = "":
+var PLAYER_NAME = "";
+
+var PLAYER_STATS = {};
+var PLAYER_ADVANVEMENTS = {};
+
+
+function setPlayer (uuid, name) {
+  PLAYER_UUID = uuid;
+  PLAYER_NAME = name;
+};
+
+
+function getStats (type) {
+  
+};
+
