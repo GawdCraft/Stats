99166542d799fdc6da0d5b940f410102187a91a2
Vinnie <vmatronia@gmail.com>
2025-08-28 22:26:19 -0400

    Update API_Config.js

--- assets/cfg/API_Config.js
+++ assets/cfg/API_Config.js
@@ -1,20 +1,20 @@
 /* GawsStats API Config */
 
 // Stats Database API
-var API_URL_DB_STATS = "https://gawdscape.x10.mx/api/stats";
+const URL_DB_STATS = "https://gawdscape.x10.mx/api/stats";
 
 // Banned Players API
-var API_URL_DB_BANS = "https://gawdscape.x10.mx/api/bans";
+const URL_DB_BANS = "https://gawdscape.x10.mx/api/bans";
 
 // Player Avatar URL
-var API_URL_PLAYER_IMG = "https://gawdscape.x10.mx/api/avatar%PLAYER%/%SIZE%
+const URL_PLAYER_IMG = "https://gawdscape.x10.mx/api/avatar%PLAYER%/%SIZE%
 
 // Player ID URL
-var API_URL_PLAYER_ID = "https://gawdscape.x10.mx/api/player/%PLAYER$";
+const URL_PLAYER_ID = "https://gawdscape.x10.mx/api/player/%PLAYER$";
 
 
 // Servers to Display
-var API_SERVERS = [
+const MC_SERVERS = [
 //  Server_Name: [
 //    "World_Name",
   //  ],
@@ -32,18 +32,24 @@ var API_SERVERS = [
 
 
 // true=only show listed / false=show all but listed
-var API_STATS_MODE = false;
+const CFG_STATS_MODE = false;
 
 // Stats to Display
-var API_STATS = [
-  "Stat_ID",
+const CFG_STATS = [
+//  "Stat_ID",
 ];
 
 
 // true=only show listed / false=show all but listed
-var API_ADVANCEMENTS_MODE = false;
+const CFG_ADVANCEMENTS_MODE = false;
 
 // Stats to Display
-var API_ADVANCEMENTS = [
-  "ADVANCEMENT_ID",
+const CFG_ADVANCEMENTS = [
+//  "ADVANCEMENT_ID",
 ];
+
+
+const PAGE_PLAYERS = 25;
+
+
+
